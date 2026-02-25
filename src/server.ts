7b6a9622067777786026495afa7b3435256a1d3a
arif-fahrizal <fahrizalarif07@gmail.com>
2026-02-25 09:02:17 +0700

    [FEAT] Create Config folder and create db.ts for connecting to mongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,13 @@
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
 import app from './app';
+import { connectDB } from './config/db';
 
 dotenv.config();
 
 const PORT = process.env.PORT || '5001';
-const MONGO_URI = process.env.MONGO_URI || '';
 
-mongoose
-  .connect(MONGO_URI)
+connectDB()
   .then(() => {
-    console.log('MONGODB CONNECTED SUCCESSFULLY');
     app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
   })
   .catch(error => console.log('ERROR CONNECTING TO MONGODB', error));
