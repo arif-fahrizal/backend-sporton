3ea9eaf10c2aba21a487a545c8e92da24fa2ed12
arif-fahrizal <fahrizalarif07@gmail.com>
2026-02-25 08:22:58 +0700

    [FEAT] Security: Implement Helmet, Rate Limiting and Cors protection

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import cors from 'cors';
 import express, { Application } from 'express';
+import ExpressMongoSanitize from 'express-mongo-sanitize';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 import path from 'node:path';
 import authRoutes from './routes/auth.routes';
 import bankRoutes from './routes/bank.routes';
@@ -9,7 +12,25 @@ import tranasctionRoutes from './routes/transaction.routes';
 
 const app: Application = express();
 
-app.use(cors());
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(helmet());
+app.use(limiter);
+app.use(cors(corsOptions));
+app.use(ExpressMongoSanitize());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
