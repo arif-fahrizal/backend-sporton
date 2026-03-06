6d7b9aabfbff74fd993387a5af4d3e019c8ad96d
arif-fahrizal <fahrizalarif07@gmail.com>
2026-03-06 15:13:35 +0700

    [ADD] File vercel.json

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -3,7 +3,7 @@ import { signInUser, signUpUser } from '../controllers/auth.controller';
 
 const router: Router = Router();
 
-router.post('/sign-in', signInUser);
 router.post('/sign-up', signUpUser);
+router.post('/sign-in', signInUser);
 
 export default router;
